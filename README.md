4f75ada6ce8d33ec288ab4c90fa5b6d13681eff6
Anders Ramsay <andersr@gmail.com>
2015-08-13 09:53:37 -0400

    original version

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tic Tac Toe Game
+
+This is a Tic Toe Game I am completing as an assignment for a school application.
---
+++ js/game.js
@@ -0,0 +1,61 @@
+// // Initate new game
+// function newGame(playerXname, playerOname, gridSize) {
+
+// 	setPlayerName(playerXname, playerOname);
+	
+// 	createGameBoard(gridSize);
+
+// 	displayGameBoard(gameBoard);
+
+// 	nextTurn = x;
+
+// 	playGame(nextTurn);
+
+// 	// set total turns
+// }
+
+
+// function playGame(nextTurn) {
+
+// }
+
+
+// function setNextTurn(previouTurn) {
+// 	if previousTurn = x {
+// 		nextTurn = o;
+// 	} else {
+// 		nextTurn = x;
+// 	}
+
+// 	return nextTurn;
+// }
+
+// function setPlayerName(playerXname, playerOname) {
+// 	//if player name field has values, set as "<name> (X)"
+// 	// else set as "Player X"
+// 	return Player-X, Player-O;
+// }
+// //create an array base on the board size
+// function createGameBoard(gridSize) {
+// 	//var gameBoard = (gridSize * gridSize) - 1;
+// 	var gameBoard = new Array(gridSize);
+// 	return gameBoard;
+// }
+
+// function displayGameBoard(gameBoard) {
+
+// //assign locations to each square for display purposes
+// //generate the table rows/columns, assign id values, and associate the id with the corresponding array id
+// // initially, just hard code all this
+
+// }
+
+// function gameSquare(location, ) {
+
+// }
+
+
+// function quitGame() {
+// 	//display an alert - are you sure?  if yes, return to index
+
+// }
